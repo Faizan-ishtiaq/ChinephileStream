005aed8072b823ea0c05b4a50f0fe224f27fbd97
Faizan Ishtiaq <faizanishtiaqwork@gmail.com>
2026-08-13 01:45:15 +0500

    update

--- src/components/Recommend.jsx
+++ src/components/Recommend.jsx
@@ -12,7 +12,7 @@ const Recommend = () => {
     setReply("");
 
     try {
-     const response = await fetch("https://web-production-01a71.up.railway.app/recommend", {
+     const response = await fetch("https://cinephile-stream-backend-ooc9.vercel.app/recommend", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message }),
